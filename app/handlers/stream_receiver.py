22e0bc0dab887544563079103f4a0cb81c92c835
marcosricciardi12 <marcosricciardi12@gmail.com>
2025-05-20 04:18:47 -0300

    Removed some prints

--- app/handlers/stream_receiver.py
+++ app/handlers/stream_receiver.py
@@ -3,7 +3,6 @@ import redis.asyncio as redis
 from app.libs.api_sigasiga_rest import ApiSigaSigaRest
 
 async def stream_receiver(websocket: WebSocket, redis_client: redis.Redis):
-    print(f"📺 Streaming iniciado para {str(websocket)}")
     await websocket.accept()
     ws_id = websocket._headers.get("sec-websocket-key")
     api_sigasiga_rest = ApiSigaSigaRest()
@@ -12,19 +11,17 @@ async def stream_receiver(websocket: WebSocket, redis_client: redis.Redis):
     token = query_params.get("token")
     valid_client = await api_sigasiga_rest.validate_and_start_client(token, ws_id)
     client_input_video_buffer = f"{event_id}-chunks_data_input_buffer-{ws_id}"
-    print("client_input_video_buffer: ", client_input_video_buffer)
 
     if not valid_client:
         print(f"🔌 Cliente no autorizado para el stream! {str(websocket)}")
         await websocket.close(code=1008)
         return
     try:
+        print(f"📺 Streaming iniciado: Cliente {str(websocket)} - Evento {event_id}")
         while True:
             data = await websocket.receive_bytes()
-            print("len data: ", len(data))
-            print("client_input_video_buffer: ", client_input_video_buffer)
             await redis_client.rpush(client_input_video_buffer, data)
 
     except WebSocketDisconnect:
         disconnect_status = await api_sigasiga_rest.disconnect_client(token, ws_id)
-        print(f"🔌 Cliente desconectado del stream {ws_id}. Status: {disconnect_status}")
+        print(f"🔌 Cliente desconectado del stream {ws_id}. Evento {event_id}. Status: {disconnect_status}")
